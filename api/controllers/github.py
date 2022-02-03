8c1f9e5268f8ae7212c877a4c49f7cd3ee3e0569
Luann <luann.bertaud@epitech.eu>
2022-02-03 10:07:08 +0100

    API-M: WIP refactoring db and deserialization: parsing github request and selecting Action

--- api/controllers/github.py
+++ api/controllers/github.py
@@ -1,16 +1,58 @@
 #!/usr/bin/env python3
 
-from tools.fomarting import ensure_json
-from tools.tokens import get_tokens, tokens_reload
-from tools.env import TWITTER_CLIENT_ID, TWITTER_CLIENT_SECRET
-from models.area import Trigger
+# from tools.fomarting import ensure_json
+# from tools.tokens import get_tokens, tokens_reload
+# from tools.env import TWITTER_CLIENT_ID, TWITTER_CLIENT_SECRET
+# from models.area import Trigger
+from flask import request
+from peewee import DoesNotExist
+from models.db import Actions, Users
 
-class GitHubTrigger(Trigger):
+# class GitHubTrigger(Trigger):
 
-    def __init__(self, toTrigger=[], uuid=None) -> None:
-        super().__init__("github", toTrigger=toTrigger, uuid=uuid)
+#     def __init__(self, toTrigger=[], uuid=None) -> None:
+#         super().__init__("github", toTrigger=toTrigger, uuid=uuid)
 
-    def pull(self):
-        # print(request.json)
-        self.execute([])
-        return {"code" : 200}
+#     def pull(self):
+#         # print(request.json)
+#         self.execute([])
+#         return {"code" : 200}
+
+def githubHook():
+    data = request.json
+    repo_name = data["repository"]["name"]
+    repo_owner = data["repository"]["owner"]["login"]
+    area_user = None
+    area_repo = None
+
+    print(f"Owner:[{repo_owner}] Repo:[{repo_name}]")
+
+    try:
+        query = Users.select().where(Users.github.is_null(False))
+        if not query:
+            raise DoesNotExist("Empty query")
+    except DoesNotExist as e:
+        return {"code": 200, "message": "received"}
+    
+    for u in query:
+        if u.github["login"] == repo_owner:
+            area_user = u.uuid
+    if area_user == None:
+        return {"code": 201, "message": "received"}
+
+    try:
+        query = Actions.select().where(Actions.type == "github", Actions.user_uuid == area_user)
+        if not query:
+            raise DoesNotExist("Empty query")
+    except DoesNotExist as e:
+        return {"code": 202, "message": "received"}
+
+    for a in query:
+        if a.content["repository"] == repo_name:
+            area_repo = a.uuid
+    if not area_repo:
+        return {"code": 203, "message": "received"}
+
+    # a.execute
+    print(f"{area_repo}")
+    return {"code": 204, "message": "received"}
--- api/models/db.py
+++ api/models/db.py
@@ -4,6 +4,7 @@ from distutils.text_file import TextFile
 from peewee import Model as __Model
 from peewee import AutoField, TextField, UUIDField
 from playhouse.postgres_ext import JSONField
+from sqlalchemy import JSON
 from models.db_globals import db
 
 class __BaseModel(__Model):
@@ -14,19 +15,21 @@ class DBdata(__BaseModel):
     version = TextField()
 
 class Users(__BaseModel):
-    id = AutoField()
+    uuid = AutoField()
     name = TextField()
     password = TextField(null=True)
     email = TextField(null=True)
-    twitterTokens = JSONField(null=True)
+    twitter = JSONField(null=True)
+    github = JSONField(null=True)
 
 class Reactions(__BaseModel):
     uuid = TextField(primary_key=True)
     type = TextField()
-    user = TextField()
+    user_uuid = TextField()
 
 class Actions(__BaseModel):
     uuid = TextField(primary_key=True)
     type = TextField()
-    user = TextField()
+    user_uuid = TextField()
+    content = JSONField(null=True)
     children = JSONField(null=True)
