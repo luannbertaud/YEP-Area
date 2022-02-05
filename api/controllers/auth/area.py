684a1e6a08ed80448ade8f29cdc23d41db65fd3e
Luann <luann.bertaud@epitech.eu>
2022-02-05 12:42:57 +0100

    API-A: New endpoint for loging in area user

--- api/controllers/auth/area.py
+++ api/controllers/auth/area.py
@@ -12,7 +12,7 @@ from datetime import datetime, timedelta
 areaAuthBP = Blueprint("areaAuthBP", __name__)
 
 
-def __validate_data(data):
+def __validate_data_register(data):
     try:
         assert(isinstance(data, dict))
         assert(isinstance(data["user_name"], str))
@@ -22,12 +22,26 @@ def __validate_data(data):
         return False
     return True
 
+def __validate_data_login(data):
+    try:
+        assert(isinstance(data, dict))
+        if ("user_name" in data.keys()):
+            assert(isinstance(data["user_name"], str))
+        assert(isinstance(data["user_password"], str))
+        if ("user_email" in data.keys()):
+            assert(isinstance(data["user_email"], str))
+        if ("user_email" not in data.keys()) and ("user_name" not in data.keys()):
+            return False
+    except:
+        return False
+    return True
+
 
 @areaAuthBP.route("/register", methods=["POST"])
 def user_register():
     data = request.json
     u = None
-    if (not __validate_data(data)):
+    if (not __validate_data_register(data)):
         return {"code": 400, "message": "Malformed JSON payload."}
 
     try:
@@ -59,28 +73,41 @@ def user_register():
 def verify():
     return {'code': 200, 'message': 'Token valid.'}
 
-# @authBP.route("login", methods=["POST"])
-# def user_login():
-#     email = request.args.get("email")
-#     password = request.args.get("password")
-#     if check_is_google_user(email):
-#         return {
-#             "status": 401,
-#         }
-#     user = check_login(email, password)
-#     if email is None or password is None or not user:
-#         return {
-#             "msg": "Invalid password or username",
-#             "status": 401
-#         }
-#     access_token = create_access_token(identity=email)
-#     return {
-#         "msg": "Login succesfull",
-#         "status": 200,
-#         "access_token": access_token,
-#         "username": user.name,
-#         "user_email": user.email
-#     }
+@areaAuthBP.route("/login", methods=["POST"])
+def user_login():
+    data = request.json
+    u = None
+    if (not __validate_data_login(data)):
+        return {"code": 400, "message": "Malformed JSON payload. (user_name or user_email, + user_password)."}
+
+    if ("user_name" in data.keys()):
+        try:
+            u = Users.get(Users.name == data['user_name'])
+        except DoesNotExist as e:
+            return {"code": 400, "message": "Area user not found with provided credentials."}
+    else:
+        try:
+            u = Users.get(Users.email == data['user_email'])
+        except DoesNotExist as e:
+            return {"code": 400, "message": "Area user not found with provided credentials."}
+
+    if not u:
+        return {"code": 500, "message": "Area user not found with provided credentials."}
+    if u.password != hashlib.sha256(data['user_password'].encode()).hexdigest():
+        return {"code": 400, "message": "Area user and password mismatch."}
+
+    payload = {
+        'user_uuid': u.uuid,
+        'exp': datetime.utcnow() + timedelta(seconds=int(JWT_VALIDITY_DELTA)),
+    }
+    return {
+        "message": "Login succesfull.",
+        "code": 200,
+        "access_token": jwt.encode(payload, JWT_SECRET, "HS256"),
+        "user_name": u.name,
+        "user_uuid": u.uuid,
+        "user_email": u.email,
+    }
 
 # @authBP.route("logout", methods=["POST"])
 # @jwt_required()
