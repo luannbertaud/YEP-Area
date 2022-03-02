9f18fe18b6d826e593713d79889bf7c744ba00dd
pablo <pablo.etienne@epitech.eu>
2022-03-02 13:41:45 +0100

    wip

--- web/src/App.js
+++ web/src/App.js
@@ -2,6 +2,7 @@ import {Route, Routes} from "react-router-dom";
 import Homepage from './pages/Homepage'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Profil from './pages/Profil'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Homepage/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
+        <Route path="/profil" element={<Profil/>}/>
       </Routes>
     </div>
   );
