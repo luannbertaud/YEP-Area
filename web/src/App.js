5090e7e78a0bcaf89e287350799d16b25ae571c4
Vandop <nguyen-van.hoang@epitech.eu>
2022-03-04 18:17:24 +0100

    Rm wrong import

--- web/src/App.js
+++ web/src/App.js
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import Homepage from './pages/Homepage'
 import Login from './pages/Login'
 import Register from './pages/Register'
-import Profil from './pages/Profil'
 import Apk from './pages/Apk'
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/profil" element={<Profil />} />
         <Route path="/dlapk" element={<Apk />} />
       </Routes>
     </div>
